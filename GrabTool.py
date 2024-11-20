6e4e8d67283f6ca86175201e6ab19ff20e2823b0
Nickyy <123208770+Nickyyrl@users.noreply.github.com>
2024-11-20 10:07:36 +0100

    V.0.1

--- GrabTool.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-
-print("Déplacez votre souris sur l'élément cible, puis appuyez sur Ctrl+C pour arrêter.")
-try:
-    while True:
-        x, y = pyautogui.position()
-        print(f"Position de la souris : X={x}, Y={y}", end="\r")
-except KeyboardInterrupt:
-    print("\nTerminé.")
